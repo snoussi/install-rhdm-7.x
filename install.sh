081da222808d9c23813847a5803a458e1199a86c
Rachid Snoussi <rachid@redhat.com>
2019-08-19 11:11:42 +0200

    updated to v 7.4.1

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-PRODUCT="Red Hat Decision Manager 7.4 on EAP 7.2"
+PRODUCT="Red Hat Decision Manager 7.4.1 on EAP 7.2"
 
-RHDM_DC=rhdm-7.4.0-decision-central-eap7-deployable
-RHDM_DS=rhdm-7.4.0-kie-server-ee8
+RHDM_DC=rhdm-7.4.1-decision-central-eap7-deployable
+RHDM_DS=rhdm-7.4.1-kie-server-ee8
 #RHDM_PATCH_WILDCARD=
 
 EAP=jboss-eap-7.2.0
