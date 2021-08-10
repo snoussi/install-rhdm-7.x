0dad63d5a4e07284cf32a98e3b657b0b7278306a
Rachid Snoussi <rachid@redhat.com>
2021-08-10 10:59:30 +0200

    updated to v7.11.1

--- add-system-properties.sh
+++ add-system-properties.sh
@@ -15,3 +15,10 @@ then
 	echo "Executing CLI script: " $CLI_SCRIPT
 	$JBOSS_HOME/bin/jboss-cli.sh -c --file=$CLI_SCRIPT
 fi
+
+echo "Update JVM setting in /bin/standalone.config"
+echo "Alternatively, you can use the following tool: https://access.redhat.com/labs/jvmconfig/ "
+
+# JAVA_OPTS="-Xms2G -Xmx2G -XX:MetaspaceSize=96M -XX:MaxMetaspaceSize=1024m -Djava.net.preferIPv4Stack=true"
+# JAVA_OPTS="$JAVA_OPTS -Djboss.modules.system.pkgs=$JBOSS_MODULES_SYSTEM_PKGS -Djava.awt.headless=true"
+# JAVA_OPTS="$JAVA_OPTS -server -XX:+UseParallelGC -XX:+UseParallelOldGC"
